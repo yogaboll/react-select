faefaf92ef320faf481ae2d07cc5dec46b145533
Charles Lee <clee@cli-26116.office.atlassian.com>
2017-08-29 14:14:22 +1000

    initial rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+
+import babel from 'rollup-plugin-babel';
+import cjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve'
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+
+export default {
+    input: 'src/index.js',
+    targets: [
+        {
+          name: 'react-select',
+          dest: 'rollupDist/react-select.js',
+          format: 'umd',
+        }, {
+          dest: 'rollupLib/index.js',
+          format: 'es',
+        }
+    ],
+    plugins: [
+      babel({
+        babelrc: false,
+        exclude: 'node_modules/**',
+        presets: ['es2015-rollup', 'stage-0', 'react'],
+        plugins: ['external-helpers']
+      }),
+      cjs({
+        include: [
+          'node_modules/classnames/**',
+          'node_modules/react-input-autosize/**',
+          'node_modules/prop-types/**',
+          'node_modules/create-react-class/**',
+          'node_modules/fbjs/**',
+          'node_modules/object-assign/**',
+          'node_modules/react/**',
+          'node_modules/react-dom/**',
+        ],
+        namedExports: {
+          'node_modules/react/react.js': ['Children', 'Component'],
+        }
+      }),
+      resolve(),
+    ]
+};
